657f22ec101b6f50e0ea4d32f88f0c403b8d5154
cepotztl <44232864+cepotztl@users.noreply.github.com>
2019-03-02 13:47:36 +0700

    Update autosafelink.js

--- autosafelink.js
+++ autosafelink.js
@@ -131,7 +131,7 @@ function auto_safeconvert() {
         }
         if (a_to_vh == false) {
             var encryptedUrl = Base64.encode(a_to_vi);
-            a_to_ve[i].href = "http://vgba.xtgem.com/index?r=" + encryptedUrl;
+            a_to_ve[i].href = "http://bandit.fastblog.net/safelink/?url=" + encryptedUrl;
             a_to_ve[i].rel = "nofollow";
             a_to_vb++;
             a_to_vc += i + ":::" + a_to_ve[i].href + "\n";
